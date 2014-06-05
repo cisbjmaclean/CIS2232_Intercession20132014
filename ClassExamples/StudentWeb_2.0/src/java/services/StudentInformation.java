7c525c55e165d673c2c4e8c151b5f9a25fb9c08f
cisbjmaclean <bjmaclean@hollandcollege.com>
2014-06-05 14:28:05 -0300

    Housecleaning
    Cleaning class example projects

---
+++ ClassExamples/StudentWeb_2.0/src/java/services/StudentInformation.java
@@ -0,0 +1,23 @@
+package services;
+
+import business.Student;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author BJ
+ */
+@WebService(serviceName = "StudentInformation")
+public class StudentInformation {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "get")
+    public String get(String txt) {
+        Student temp = new Student(txt, "A","B","C");
+        return temp.toStringXML();
+    }
+}
